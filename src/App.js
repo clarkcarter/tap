6fb907d3ba09d655b8e2538193923a53fb8a2823
Clark Carter <clark@clarkcarter.com>
2017-11-02 16:31:32 +1100

    add mapbox and sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import { Sidebar, Tab } from 'react-leaflet-sidebarv2';
 import './App.css';
 
-const stamenTonerTiles = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-const mapCenter = [39.9528, -75.1638];
-const zoomLevel = 2;
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoiY2xhcmtjYXJ0ZXIiLCJhIjoiY2lncTBtNjMyMDFvNHY2a29rOWdtY2RwaCJ9.TX02M_EHzs8lUyUacPrOdQ"
+});
+
 
 class App extends React.Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     collapsed: false,
+     selected: 'home',
+   };
+ }
+
+ onClose() {
+   this.setState({collapsed: true});
+ }
+ onOpen(id) {
+   this.setState({
+     collapsed: false,
+     selected: id,
+   })
+ }
+
   render() {
     return (
       <div>
+        <Sidebar id="sidebar" collapsed={this.state.collapsed} selected={this.state.selected}
+                   onOpen={this.onOpen.bind(this)} onClose={this.onClose.bind(this)}>
+            <Tab id="home" header="Home" icon="fa fa-home">
+              <p>No place like home!</p>
+            </Tab>
+            <Tab id="settings" header="Settings" icon="fa fa-cog" anchor="bottom">
+              <p>Settings dialogue.</p>
+            </Tab>
+          </Sidebar>
         <Map
-            center={mapCenter}
-            zoom={zoomLevel}
-        >
-            <TileLayer
-                attribution={stamenTonerAttr}
-                url={stamenTonerTiles}
-            />
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "100vh",
+            width: "100vw"
+        }}>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          </Layer>
         </Map>
       </div>
     );
